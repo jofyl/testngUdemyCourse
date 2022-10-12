23c5e687b20f4d1771e48065f1a7eb0d348e7058
Joseph Loutfi <joseph@anghami.com>
2022-10-12 12:01:40 +0400

    chore: add info on soft asserts

---
+++ project/project/src/test/java/com/project/tests/EBayHomeSanitySoftAssert.java
@@ -0,0 +1,27 @@
+package com.project.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class EBayHomeSanitySoftAssert {
+
+    private SoftAssert sa = new SoftAssert();
+
+    @Test
+    public void emptySearchTest() {
+        sa.assertEquals(1, 2, "hello assert 1");
+        sa.assertEquals(1, 1, "hello assert 2");
+
+        sa.assertAll();
+    }
+
+    @Test
+    public void emptySearchTest2() {
+        sa.assertEquals(1, 2, "hello asset 3");
+        
+        sa.assertAll();
+    }
+
+}
